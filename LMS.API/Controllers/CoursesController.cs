4b971c6250a9ae5e8a5956994c2d004515681c08
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-09-17 14:51:45 +0200

    WIP. Add basic structure for [HttpPut("{id}")] and [HttpDelete("{id}")]

--- LMS.API/Controllers/CoursesController.cs
+++ LMS.API/Controllers/CoursesController.cs
@@ -41,4 +41,64 @@ namespace LMS.API.Controllers
 
         //return Ok(movieDto);
     }
+
+    // PUT: api/Courses/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateCourse(int id, CourseToUpdateDto dto)
+    {
+        //ToDo Jimmy [HttpPut("{id}")]
+        //if (dto.Rating < 0 || dto.Rating > 10)
+        //{
+        //    return BadRequest("Rating must be between 0 and 10.");
+        //}
+
+        //// Hämta den befintliga filmen från databasen
+        //var existingMovie = await _context.Movie
+        //    .Include(m => m.Director)
+        //    .Include(m => m.Actors)
+        //    .Include(m => m.Genres)
+        //    .FirstOrDefaultAsync(m => m.Id == id);
+
+        //if (existingMovie == null) return NotFound();
+
+
+        //// Use AutoMapper to update the entity
+        //_mapper.Map(dto, existingMovie);
+
+        //try
+        //{
+        //    await _context.SaveChangesAsync();
+        //}
+        //catch (DbUpdateConcurrencyException)
+        //{
+        //    if (!MovieExists(id))
+        //    {
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        throw;
+        //    }
+        //}
+
+        //return NoContent();
+    }
+
+    // DELETE: api/Courses/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCourse(int id)
+    {
+        //var movie = await _context.Movie.FindAsync(id);
+        //if (movie == null)
+        //{
+        //    return NotFound();
+        //}
+
+        //_context.Movie.Remove(movie);
+        //await _context.SaveChangesAsync();
+
+        //return NoContent();
+    }
+
+    
 }
