4ce61d7b7fce915aff566224fc5a2419ef2d0a92
marcus pedersen <marcus@mpedersen.me>
2024-09-17 14:21:49 +0200

    Rename User entity and update property types
    - Renamed entity from `Users` to `User` for consistency with single instance representation
    - Changed property type from `Modules Module` to `public Module Modules { get; set; }` for improved clarity and consistency

---
+++ LMS.API/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult
+    }
+}
