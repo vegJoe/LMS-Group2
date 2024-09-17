979b8764dd42e60cf63bd79702d8043d7f06a31a
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-09-17 14:44:58 +0200

    WIP. Add basic structure for api/Courses/{id}

--- LMS.API/Controllers/CoursesController.cs
+++ LMS.API/Controllers/CoursesController.cs
@@ -8,17 +8,37 @@ namespace LMS.API.Controllers
     [ApiController]
     public class CoursesController : ControllerBase
     {
+        // ToDo database?
     }
 
     // GET: api/Courses
     [HttpGet]
     public async Task<ActionResult<IEnumerable<CourseDto>>> GetCourses()
     {
+        //ToDo Jimmy api/Courses
         //var actorsDto = await _context.Actor.ProjectTo<ActorDto>(_mapper.ConfigurationProvider).ToListAsync();
 
         //if (actorsDto == null) return NotFound();
 
         //return Ok(actorsDto);
-        
+
+    }
+
+    // GET: api/Courses/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CourseDto>> GetCourse(int id)
+    {
+        //ToDo Jimmy api/Courses/{id}
+        //var movie = await _context.Movie.FindAsync(id);
+
+        //if (movie == null)
+        //{
+        //    return NotFound();
+        //}
+
+        //// Mappa från Movie till MovieDto
+        //var movieDto = _mapper.Map<MovieDto>(movie);
+
+        //return Ok(movieDto);
     }
 }
