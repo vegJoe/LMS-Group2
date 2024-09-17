064932aa7c13a412a2b1bd79344df4142721759e
Karsten Tvarnö <vvarrior@gmail.com>
2024-09-17 11:21:13 +0200

    Added tables for LMS database

---
+++ LMS.API/Models/Entities/Courses.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace LMS.API.Models.Entities
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public ICollection<Users> Users { get; set; }
+        public ICollection<Modules> Modules { get; set; }
+    }
+}
---
+++ LMS.API/Models/Entities/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.API.Models.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int CourseId { get; set; }
+        public Courses Course { get; set; }
+    }
+}
