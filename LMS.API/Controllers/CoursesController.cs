453d66e817ffc13f14de9a30904c9e65cf2c7293
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-09-17 14:37:02 +0200

    WIP. Add CoursesController, basic structure for api/Courses...
    - Have not yet implemented connectin to DB.
    - Created CourseDto.

---
+++ LMS.API/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using LMS.API.Models.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+    }
+
+    // GET: api/Courses
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CourseDto>>> GetCourses()
+    {
+        //var actorsDto = await _context.Actor.ProjectTo<ActorDto>(_mapper.ConfigurationProvider).ToListAsync();
+
+        //if (actorsDto == null) return NotFound();
+
+        //return Ok(actorsDto);
+        
+    }
+}
---
+++ LMS.API/Models/Dtos/CourseDto.cs
@@ -0,0 +1,8 @@
+﻿using LMS.API.Models.Entities;
+
+namespace LMS.API.Models.Dtos
+{
+    
+    public record CourseDto(string Name,  string Description, DateTime StartDate, IEnumerable<Users> Users, IEnumerable<Modules> Modules);
+    
+}
