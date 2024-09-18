667633dde100a7633b0b696cff934c91b205754c
marcus pedersen <marcus@mpedersen.me>
2024-09-18 16:57:57 +0200

    Resolve conflicts

--- LMS.API/Models/Dtos/UserDto.cs
+++ LMS.API/Models/Dtos/UserDto.cs
@@ -5,11 +5,12 @@ namespace LMS.API.Models.Dtos
     public class UserDto
     {
         public Guid Id { get; set; }
-        public string? RefreshToken { get; set; }
-        public DateTime RefreshTokenExpireTime { get; set; }
+        public string UserId { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public int? CourseId { get; set; }
-        public Course Course { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int CourseId { get; set; }
+        public Course? Course { get; set; }
     }
 }
