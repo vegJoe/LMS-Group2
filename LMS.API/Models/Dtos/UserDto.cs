8fbda9d9f0384bf22e0b2541438068ede3ec0a9c
marcus pedersen <marcus@mpedersen.me>
2024-09-18 17:03:18 +0200

    Revert "Resolve conflicts"
    This reverts commit 667633dde100a7633b0b696cff934c91b205754c.

--- LMS.API/Models/Dtos/UserDto.cs
+++ LMS.API/Models/Dtos/UserDto.cs
@@ -5,12 +5,11 @@ namespace LMS.API.Models.Dtos
     public class UserDto
     {
         public Guid Id { get; set; }
-        public string UserId { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpireTime { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public int CourseId { get; set; }
-        public Course? Course { get; set; }
+        public int? CourseId { get; set; }
+        public Course Course { get; set; }
     }
 }
