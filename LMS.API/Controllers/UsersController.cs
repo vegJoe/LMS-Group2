1bc0d6faff5394e5290050bc03d76511eee23beb
marcus pedersen <marcus@mpedersen.me>
2024-09-19 13:34:28 +0200

    Use AutoMapper to return UserDtos and make the method async

--- LMS.API/Controllers/UsersController.cs
+++ LMS.API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
-﻿using LMS.API.Data;
+﻿using AutoMapper;
+using LMS.API.Data;
 using LMS.API.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,24 +13,25 @@ namespace LMS.API.Controllers
     public class UsersController : ControllerBase
     {
         private readonly LMSApiContext _context;
-        public UsersController(LMSApiContext context)
+        private readonly IMapper _mapper;
+        public UsersController(LMSApiContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
         // GET: api/<UsersController>
         [HttpGet]
-        public IEnumerable<UserDto> Get()
+        public async Task<ActionResult<IEnumerable<UserDto>>> Get()
         {
-            return _context.Users.Select(x => new UserDto
+            var users = await _context.Users.ToListAsync();
+
+            if (users.Count == 0)
             {
-                FirstName = x.FirstName,
-                LastName = x.LastName,
-                Email = x.Email ?? "",
-                UserId = x.Id,
-                UserName = x.UserName ?? "",
-                CourseId = x.CourseId ?? 0,
-                Course = x.Course,
-            }).ToList();
+                return NotFound("No users found.");
+            }
+
+            var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+            return Ok(userDtos);
         }
 
         //GET api/<UsersController>/5
@@ -46,6 +49,31 @@ namespace LMS.API.Controllers
                 CourseId = user.CourseId ?? 0,
                 Course = user.Course,
             };
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<UserDto> Delete(string id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+            var userDto = new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email ?? "",
+                UserId = user.Id,
+                UserName = user.UserName ?? "",
+                CourseId = user.CourseId ?? 0,
+                Course = user.Course,
+            };
+
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+                return Ok(userDto);
+            }
+
+            return BadRequest();
         }
     }
 }
