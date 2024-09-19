038c208fca4e9c5b7b2f8387904ed12dd863226b
Karsten Tvarnö <vvarrior@gmail.com>
2024-09-19 09:29:18 +0200

    Updated Entities to be required or nullable.

--- LMS.API/Models/Entities/Course.cs
+++ LMS.API/Models/Entities/Course.cs
@@ -1,12 +1,16 @@
-﻿namespace LMS.API.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.API.Models.Entities
 {
     public class Course
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        [Required]
         public DateTime StartDate { get; set; }
-        public ICollection<User> Users { get; set; }
-        public ICollection<Module> Modules { get; set; }
+        public ICollection<User>? Users { get; set; }
+        public ICollection<Module>? Modules { get; set; }
     }
 }
--- LMS.API/Models/Entities/Module.cs
+++ LMS.API/Models/Entities/Module.cs
@@ -1,12 +1,16 @@
-﻿namespace LMS.API.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.API.Models.Entities
 {
     public class Module
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+        [Required]
         public int CourseId { get; set; }
-        public Course Course { get; set; }
-        public ICollection<Activity> Activites { get; set; }
+        public Course? Course { get; set; }
+        public ICollection<Activity>? Activites { get; set; }
     }
 }
