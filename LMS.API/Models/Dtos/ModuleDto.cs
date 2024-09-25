32d9c0e3b0f64050d683e2f758ea6cb1a73a749f
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-09-25 15:04:30 +0200

    Add startdate and enddate to ModuleDto.

--- LMS.API/Models/Dtos/ModuleDto.cs
+++ LMS.API/Models/Dtos/ModuleDto.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public int CourseId { get; set; }
         //public CourseDto Course { get; set; }
         public IEnumerable<ActivityDto> Activities { get; set; }
