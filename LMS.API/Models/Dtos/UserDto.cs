c40d147ae0d048e2926e701ee2c1d0f366cfa9fa
ammarovehub <ammarove1@pm.me>
2024-09-27 09:50:36 +0200

    G2L-228 <Usernames reveal has been dealt with>

--- LMS.API/Models/Dtos/UserDto.cs
+++ LMS.API/Models/Dtos/UserDto.cs
@@ -5,7 +5,6 @@
         public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
         public int CourseId { get; set; }
