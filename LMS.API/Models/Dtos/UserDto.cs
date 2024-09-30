90d7df4c0688996eebc4388a18d27888be13fbc6
ammarovehub <ammarove1@pm.me>
2024-09-30 10:49:25 +0200

    Revert "G2L-228 <Usernames reveal has been dealt with>"
    This reverts commit c40d147ae0d048e2926e701ee2c1d0f366cfa9fa.

--- LMS.API/Models/Dtos/UserDto.cs
+++ LMS.API/Models/Dtos/UserDto.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
         public int CourseId { get; set; }
